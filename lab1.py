95bd05e1dc254891dc2dd12f170603bb49f58f93
Daniel <danrmunic@gmail.com>
2019-04-04 16:10:53 -0700

    added documentation

--- lab1.py
+++ lab1.py
@@ -2,14 +2,14 @@
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   if int_list is None:
-       raise ValueError()
+   if int_list is None:    
+       raise ValueError()     #raises ValueError if list is None
    
    if not int_list:
-       return None
+       return None            #returns None if list is empty
    
-   max = int_list[0]
-   for x in int_list:
+   max = int_list[0]          #sets initial max to be the first value in the list
+   for x in int_list:         #for loop goes through each value, makes it the new max if greater than current max
        if x > max:
            max = x
    return max
@@ -18,27 +18,28 @@ def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
    if int_list is None:
-       raise ValueError()
+       raise ValueError()     #raises ValueError if list is None
    
    if len(int_list) == 0:
-       return []
+       return []              #returns an empty list if the list is empty
 
+   #the statement below returns a list of the final value in int_list plus the reversed version of the rest of int_list
    return [int_list[len(int_list) - 1]] + reverse_rec(int_list[:len(int_list) - 1])
 
 def bin_search(target, low, high, int_list):  # must use recursion
    """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
    if int_list is None:
-       raise ValueError()
+       raise ValueError()     #raises ValueError if list is None
    
-   mid = int((low + high) / 2)
+   mid = int((low + high) / 2)#sets mid as the index halfway between low and high
    if int_list[mid] == target:
-       return mid
+       return mid             #returns mid if the value at mid in the list is the target
    
    if low > high:
-       return None
+       return None            #base case: if low is greater than high, the list can not be split in half anymore
 
-   if int_list[mid] > target:
-       return bin_search(target, low, mid-1, int_list)
+   if int_list[mid] > target: 
+       return bin_search(target, low, mid-1, int_list)#searches for the target in the lower half of the list
    elif int_list[mid] < target:
-       return bin_search(target, mid+1, high, int_list)
+       return bin_search(target, mid+1, high, int_list)#searches for the target in the upper half of the list
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -11,10 +11,15 @@ class TestLab1(unittest.TestCase):
             max_list_iter(tlist)
         
         tlist = []
-        self.assertEqual(max_list_iter(tlist), None)
+        self.assertIsNone(max_list_iter(tlist)) #test return None for an empty list
 
         tlist = [5,6,3,8,19,-4,0]
-        self.assertEqual(max_list_iter(tlist), 19)
+        self.assertEqual(max_list_iter(tlist), 19) #test for functionality
+
+        tlist = [1,2]
+        self.assertEqual(max_list_iter(tlist), 2)
+        tlist = [2,1]
+        self.assertEqual(max_list_iter(tlist), 2)  #test for endpoints
 
 
     def test_reverse_rec(self):
@@ -22,9 +27,9 @@ class TestLab1(unittest.TestCase):
         with self.assertRaises(ValueError):
             reverse_rec(tlist)
 
-        self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
-        self.assertEqual(reverse_rec([5,6,3,8,19,-4,0]), [0,-4,19,8,3,6,5])
-        self.assertEqual(reverse_rec([]), [])
+        self.assertEqual(reverse_rec([1,2,3]),[3,2,1]) #test for functionality
+        self.assertEqual(reverse_rec([5,6,3,8,19,-4,0]), [0,-4,19,8,3,6,5]) #test unsorted list 
+        self.assertEqual(reverse_rec([]), []) #test for empty list
 
 
     def test_bin_search(self):
@@ -36,9 +41,10 @@ class TestLab1(unittest.TestCase):
         low = 0
         high = len(list_val)-1
         self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
-        self.assertEqual(bin_search(-4, 0, 6, [-4, 0, 3, 5, 6, 8, 19]), 0)
-        self.assertIsNone(bin_search(3, 0, 0, [1,2,3]))
-        self.assertIsNone(bin_search(3, 0, 3, [1,2,4]))
+        self.assertEqual(bin_search(-4, 0, 6, [-4, 0, 3, 5, 6, 8, 19]), 0) #tests if it can find first endpoint
+        self.assertIsNone(bin_search(3, 0, 0, [1,2,3])) #tests if a number will not be found if it is not in the range
+        self.assertIsNone(bin_search(3, 0, 3, [1,2,4])) #tests if a number will not be found if it is not in the list
+        self.assertEqual(bin_search(19, 0, 6, [-4, 0, 3, 5, 6, 8, 19]), 6) #tests if it can find last endpoint
 
 if __name__ == "__main__":
         unittest.main()
